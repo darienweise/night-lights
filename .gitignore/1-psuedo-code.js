be5c04c123bd2239f5badffacde9e752e90b4e72
Darien <darienweise@gmail.com>
2020-09-12 19:59:02 -0400

    first commit

---
+++ .gitignore/1-psuedo-code.js
@@ -0,0 +1,27 @@
+// Landing page has title of adventure game, a story image and a button beckoning the user to start the game
+// if button is clicked, then a new image loads of a bakery
+// the user is presented with a kitchen island with 8 clickable items on top, a recipe, a cookie cutter, a rolling pin, baking sheet, icing, gumdrops, dough and a brown shaker marked 'Special Spice'.
+// The owners of the store - an old man and an woman, stand in front of the island and yell at the player of the story. They tell the player that they took the player in after their parents died, and that if the player doesn't pick up the slack then they will toss them on the streets. They ask the player to say "Yes" if they understand.
+// the player is presented with a prompt to say yes or to say no. 
+// if the player says no, then the image of the cold outdoors pops up with an alert that says - "Game over"
+// else the two owners tell the player to make 100 gingerbread men and tell the player when they finish their first batch, to call them over and they will try the cookie to be certain the player made it correctly. The owners then leave.
+//If the user clicks on the recipe, they see the steps needed to make the cookie
+// else the user gets an alert that says read the recipe first.
+// the user then clicks on each item in order of what the recipe says
+// if the user clicks out of order from the recipe then a prompt says to follow the recipe card
+// after they complete the cookie making, they add the cookie to the pan and then alerted that they have 5 more cookies to go before this batch is ready for the oven, and 99 more cookies to go.
+// when user acknowleges that they have 99 cookies more to go, the player recieves an alert that they are sick and tired of being treated badly but they will continue on. 
+// The user makes a second cookie and they are alerted that they have 4 more cookies to go before this batch is ready for the oven, and 98 more cookies to go.
+// The user is alerted again with the player's thoughts. The player says they are sick and tired of being treated badly and that they wish there was something they could do.
+// The user completes 3 more cookies and then the recipe book starts to glow and shake slightly. The user is alerted 'That is strange, why is the recipe book glowing?'
+// if the user clicks on anything other than the recipe book, alert them that they are curious about why the recipe book is glowing.
+// the user sees an astericks on the bottom of the recipe that wasn't there before. It says 'add Special Spice to give your enemies a lil' something extra.' 
+// The user then can click on the Special Spice and add extra spice to the last cookie. An alert says 'Add more'! 
+// The user adds more
+// an alert says "More spice!"
+// the user adds more.
+// The alert says "The whole bottle!"
+// the user adds the whole bottle. 
+// the user can now put the first batch of cookies in the oven and wait. 
+//the oven image begins to shake and glow
+// 
---
+++ .gitignore/2-psuedo-code.js
@@ -0,0 +1,77 @@
+
+
+// Player presses 'Start Baking!'
+// The user is alerted, 'Welcome to the bakery, to start the game click on the recipe card. Memorize the order of the ingredients and then click "READY" when you are ready to start. To win the round you must click each item in the order that the recipe card states.'
+// The user presses 'ok' to exit the alert. 
+// If the user clicks on anything but the recipe card, an alert tell the user to 'click the recipe card to start'
+// else if the user clicks the recipe card then the recipe card will ask the user "When you're ready to memorize the ingredients, press start." 
+// When the user presses start, *the round begins* and a series of ingredients will show in order on the screen for a limited duration of time
+// The user then will be allowed to click on the main images on the page of different ingredients.  
+// If the user gets the order correct, they get one point. 
+// else they get one strike.
+// if user gets three strikes they are out and an alert will show the user that they're record is (x)number of points.
+// If the user reaches 10 points, then the user wins and be alerted a congratulations for winning the cookie baking game.
+
+// *********ROUNDS***********
+// ROUND 1
+// 'THREE' clickable buttons are located on the baking table.
+// Level 1: 
+// When round starts: Flash (2) randomized images of the available buttons.
+// Level 2: 
+// When round starts: Flash (3) randomized images of the available buttons.
+// Level 3: 
+// When round starts: Flash (4) randomized images of the available buttons.
+
+// ROUND 2
+// 'FIVE' Clickable buttons now located on the baking table.
+// Level 4: 
+// When round starts: Flash (2) randomized images of the available buttons.
+// Level 5: 
+// When round starts: Flash (4) randomized images of the available buttons.
+// Level 6: 
+// When round starts: Flash (6) randomized images of the available buttons.
+
+// ROUND 3
+// 'SEVEN' clickable buttons now located on baking table
+// Level 7: 
+// When round starts: Flash (3) randomized images of the available buttons.
+// Level 8: 
+// When round starts: Flash (6) randomized images of the available buttons.
+// Level 9: 
+// When round starts: Flash (9) randomized images of the available buttons.
+// LEVEL 10 *final round
+// When round starts: Flash (12) randomized images of the available buttons.
+
+
+
+// add event listener to start baking 
+   // on user click 
+   // hide start button 
+   // show instructions 
+
+// add event listener that listens for user to begin (on begin button in the instructions)
+   // on user clicking begin
+   // hide instructions
+   // show level instruction image
+   // hide level instruction image
+   // activate buttons on page for user to click
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

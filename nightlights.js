e477bda67e3009c8fd44e4f44201c2eac6c57d28
Darien <darienweise@gmail.com>
2020-09-13 10:29:01 -0400

    first commit

---
+++ nightlights.js
@@ -0,0 +1,32 @@
+$(function(){
+         // Add event listener for user click 
+   $('button').on('click', function(){
+            $(this).button('border-color', '#ffffff');
+         })
+      })
+      
+      
+      
+         // if radio button is clicked
+         // and div with class of button is clicked
+         // make border of clicked div inherit the radio button's color
+      
+      // add conditional click listener
+         // on button.clear click
+         // and if div.button color value is true
+         // change the div.button border color to default value
+      
+      // add event listener for click on button.clear
+         // if button.clear button is clicked
+         // confirm user if they want to clear the screen
+         // if true then set all div.button classes to default colour
+         // false then do nothing.
+      
+      
+      //stretch goal: 
+      // create listener for click
+         // if any div.button is border color true
+            // confirm with user "Do you want to clear the page and add the template?"
+            // if true
+               // add template border color to specified array of div.buttons
+            // if false do nothing.
